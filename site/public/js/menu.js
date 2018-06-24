59ab20bc67bd0a93b5345605a8b37d3c70ca29d2
clement <clem.0076@gmail.com>
2018-06-24 18:00:50 +0200

    [test]

--- site/public/js/menu.js
+++ site/public/js/menu.js
@@ -2,5 +2,5 @@ $("#hamburger_menu").on("click", function (event) {
     $("#hamburger_menu").css("display", "none");
     $("#titles").css("margin-top", "192px");
     $("#name").replaceWith('<img id="star" src="public/images/STAR.png" alt="star">');
-    $("#slogan").text("designed by space.");
+    $("#slogan").text("designed by space."); 
 })
