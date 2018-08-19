b351503043c6f62726639f0cfe44fcac92e9a6a6
clement <clem.0076@gmail.com>
2018-08-19 17:57:54 +0200

    Reprise à zéro

--- site/public/js/menu.js
+++
@@ -1,6 +0,0 @@
-$("#hamburger_menu").on("click", function (event) {
-    $("#hamburger_menu").css("display", "none");
-    $("#titles").css("margin-top", "192px");
-    $("#name").replaceWith('<img id="star" src="public/images/STAR.png" alt="star">');
-    $("#slogan").text("designed by space."); 
-})
